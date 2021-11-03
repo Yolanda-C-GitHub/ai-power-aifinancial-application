20bc6f28b8c9b963bcb6fa97a7f446f507c74481
NDC <andy_chen123@hotmail.com>
2021-11-03 16:08:28 -0400

    .

--- Client/src/components/Login/Login.js
+++ Client/src/components/Login/Login.js
@@ -1,24 +1,35 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import {Form, Button, Card} from 'react-bootstrap'
 import './login.css' 
 
 export default function Login() {
+
+    const emailRef = useRef()
+    const passwordRef = useRef()
+
+    function handleSubmit(e){
+        e.preventDefault()
+        console.log(emailRef.current.value)
+        console.log(passwordRef.current.value)
+    }
+
+
     return (
         <div className='d-flex justify-content-center'>
             <Card className='loginContainer w-50'>
                 <Card.Body> 
                     <h2 className='text-center mb-5'>Login In</h2>
 
-                    <Form className='loginFormContainer'>
+                    <Form className='loginFormContainer' onSubmit = {handleSubmit}>
 
                         <Form.Group>
                             <Form.Label>E-Mail Address</Form.Label>
-                            <Form.Control type='email' placeholder='Enter email' required></Form.Control>
+                            <Form.Control type='email' placeholder='Enter email' ref={emailRef} required></Form.Control>
                         </Form.Group>
 
                         <Form.Group>
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type='password' placeholder='Enter password' required></Form.Control>
+                            <Form.Control type='password' placeholder='Enter password' ref={passwordRef} required></Form.Control>
                         </Form.Group> 
 
                         <Button type='submit'> Sign in! </Button>
