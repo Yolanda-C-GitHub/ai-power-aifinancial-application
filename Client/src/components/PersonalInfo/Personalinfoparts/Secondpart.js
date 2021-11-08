f6d13ed440edcc9e87fad6c03d2c57f39b81ddab
NDC <andy_chen123@hotmail.com>
2021-11-08 17:55:49 -0500

    working on second part

--- Client/src/components/PersonalInfo/Personalinfoparts/Secondpart.js
+++ Client/src/components/PersonalInfo/Personalinfoparts/Secondpart.js
@@ -1,32 +1,35 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import {Card, Form} from 'react-bootstrap'
 import './secondpart.css' 
 
-export default function Secondpart() {
+export default function Secondpart(params) {
+
+    const genderRef = useRef()
+    const birthdayRef = useRef()
+    const sinRef = useRef()
 
 
 
     return (
         <Card className='secondPartCard d-flex border' >
+
             <Form.Group className='secondPartFormGroupGender'>
                 <Form.Label className ='secondPartFormGroupGenderLabel'>Gender:</Form.Label>
-                <Form.Check className = 'secondPartFormGroupGenderCheck' name='genderselector' label='Male' type='radio'></Form.Check>
-                <Form.Check className = 'secondPartFormGroupGenderCheck' name='genderselector' label='Female' type='radio'></Form.Check>
+                <Form.Check className = 'secondPartFormGroupGenderCheck' name='genderselector' label='Male' type='radio' ref={genderRef} ></Form.Check>
+                <Form.Check className = 'secondPartFormGroupGenderCheck' name='genderselector' label='Female' type='radio' ref={genderRef} ></Form.Check>
             </Form.Group>
 
             <Form.Group className='secondPartFormGroupBirth'>
                 <Form.Label  className='secondPartFormGroupBirthLabel' >Date of Birth:</Form.Label>
-                <Form.Control type='date' className=''></Form.Control>
+                <Form.Control type='date' className='' ref={birthdayRef}></Form.Control>
             </Form.Group>
-
 
             <Form.Group className='secondPartFormGroupSin'>
                 <Form.Label  className='secondPartFormGroupSinLabel' >SIN#:</Form.Label>
-                <Form.Control className=''></Form.Control>
+                <Form.Control className='' ref={sinRef} onClick={()=>params.personalInfoOther.setSinNum(sinRef.current.value)}></Form.Control>
             </Form.Group>
 
-            
-
+        
 
         </Card>
     )
