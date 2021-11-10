226be49fec1a651e509aa8dc43b011145d160616
NDC <andy_chen123@hotmail.com>
2021-11-10 14:57:05 -0500

    added label

--- Client/src/components/PersonalInfo/Personalinfoparts/Thirdpart.js
+++ Client/src/components/PersonalInfo/Personalinfoparts/Thirdpart.js
@@ -1,5 +1,5 @@
 import React, {useRef} from 'react'
-import {Card, Form} from 'react-bootstrap'
+import {Card, Form, FloatingLabel} from 'react-bootstrap'
 import './thridpart.css'
 
 export default function Thirdpart(params) {
@@ -13,43 +13,47 @@ export default function Thirdpart(params) {
 
     return (
         <Card className='thridPartCard'>
-             <Form.Select className='thirdPartSelector' ref={livingStatusRef} onChange={()=>params.personalInfoStatus.setLivingStatus(livingStatusRef.current.value)}>
-                <option>Living Status</option>
-                <option value='owner'>Owner</option>
-                <option value='rent'>Rent</option>
-                <option value='with parent'>With Parent</option>
-                <option value='with other'>With Other</option>
-                <option value='other'>Other</option>
-            </Form.Select>
 
-            
-            <Form.Select className='thirdPartSelector' ref={maritalStatusRef} onChange={()=>params.personalInfoStatus.setMaritalStatus(maritalStatusRef.current.value)}>
-                <option>Marital Status</option>
-                <option value='married'>Married</option>
-                <option value='single'>Single</option>
-                <option value='divorced'>Divorced</option>
-                <option value='separated'>Separated</option>
-                <option value='widowed'>Widowed</option>
-                <option value='common law'>Common Law</option>
-                <option value='other'>Other</option>
-            </Form.Select>
-
-            <Form.Select className='thirdPartSelector' ref={citizenshipRef} onChange={()=>params.personalInfoStatus.setCitizenship(citizenshipRef.current.value)} >
-                <option>Citizenship</option>
-                <option value='canadian'>Canadian</option>
-                <option value='chinese'>Chinese</option>
-                <option value='pr'>PR</option>
-                <option value='other'>Other</option>
-            </Form.Select>            
+            <FloatingLabel label='Living Status'>
+                <Form.Select className='thirdPartSelector' ref={livingStatusRef} onChange={()=>params.personalInfoStatus.setLivingStatus(livingStatusRef.current.value)}>
+                    <option value='owner'>Owner</option>
+                    <option value='rent'>Rent</option>
+                    <option value='with parent'>With Parent</option>
+                    <option value='with other'>With Other</option>
+                    <option value='other'>Other</option>
+                </Form.Select>
+            </FloatingLabel>
 
 
-            <Form.Select className='thirdPartSelector' ref={taxStatusRef} onChange={()=>params.personalInfoStatus.setTaxStatus(taxStatusRef.current.value)}>
-                <option>Tax Status</option>
-                <option value='canada'>Canada</option>
-                <option value='usa'>USA</option>
-                <option value='both'>Both</option>
-                <option value='other'>Other</option>
-            </Form.Select>
+            <FloatingLabel label='Marital Status' >
+                <Form.Select className='thirdPartSelector' ref={maritalStatusRef} onChange={()=>params.personalInfoStatus.setMaritalStatus(maritalStatusRef.current.value)}>
+                    <option value='married'>Married</option>
+                    <option value='single'>Single</option>
+                    <option value='divorced'>Divorced</option>
+                    <option value='separated'>Separated</option>
+                    <option value='widowed'>Widowed</option>
+                    <option value='common law'>Common Law</option>
+                    <option value='other'>Other</option>
+                </Form.Select>
+            </FloatingLabel>
+
+            <FloatingLabel label='Citizenship' >
+                <Form.Select className='thirdPartSelector' ref={citizenshipRef} onChange={()=>params.personalInfoStatus.setCitizenship(citizenshipRef.current.value)} >
+                    <option value='canadian'>Canadian</option>
+                    <option value='chinese'>Chinese</option>
+                    <option value='pr'>PR</option>
+                    <option value='other'>Other</option>
+                </Form.Select>            
+            </FloatingLabel>
+
+            <FloatingLabel label='Tax Status' >
+                <Form.Select className='thirdPartSelector' ref={taxStatusRef} onChange={()=>params.personalInfoStatus.setTaxStatus(taxStatusRef.current.value)}>
+                    <option value='canada'>Canada</option>
+                    <option value='usa'>USA</option>
+                    <option value='both'>Both</option>
+                    <option value='other'>Other</option>
+                </Form.Select>
+            </FloatingLabel>
 
         </Card>
     )
