22a9684f125fd492a451abbb1754a70b22180deb
NDC <andy_chen123@hotmail.com>
2021-11-18 14:03:28 -0500

    working on side menu

--- Client/src/components/Sidemenu/SidemenuData.js
+++ Client/src/components/Sidemenu/SidemenuData.js
@@ -1,11 +1,33 @@
  import React from 'react'
- 
+ import HomeIcon from '@mui/icons-material/Home';
+ import ContactsIcon from '@mui/icons-material/Contacts';
+ import FamilyRestroomIcon from '@mui/icons-material/FamilyRestroom';
+ import WorkIcon from '@mui/icons-material/Work';
+
+
+
+
  export const SidemenuData = [
     {
         title: 'Home',
-        icon: <Icon />,
+        icon: <HomeIcon />,
         link:'/home'
-    }
+    },
+    {
+        title: 'Personal Info',
+        icon: <ContactsIcon />,
+        link:'/home'
+    },
+    {
+        title: 'Family Info',
+        icon: <FamilyRestroomIcon />,
+        link:'/home'
+    },
+    {
+        title: 'Employment Info',
+        icon: <WorkIcon />,
+        link:'/home'
+    },
  ]
    
  
