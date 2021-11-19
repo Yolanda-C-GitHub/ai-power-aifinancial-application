c4e5cddaa5e39dceed8c5e2290ac0f7453b991d3
NDC <andy_chen123@hotmail.com>
2021-11-19 11:40:32 -0500

    working on sidemenu

--- Client/src/components/Sidemenu/Sidemenu.js
+++ Client/src/components/Sidemenu/Sidemenu.js
@@ -1,21 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './sidemenu.css'
 import {SidemenuData} from './SidemenuData'
 import {Link} from 'react-router-dom'
 import {Navbar, Container} from 'react-bootstrap'
 import MenuIcon from '@mui/icons-material/Menu';
-
+import CloseIcon from '@mui/icons-material/Close';
 
 export default function Sidemenu() {
+    
+    const [sidemenu, setSidemenu] = useState(false);
+
+    const showSidemenu = () => setSidemenu(!sidemenu)
+
+
+
+
     return (
         <div>
-
+    
             <Navbar className ='navBar'>
-                <MenuIcon />
-                <h1>AI Financial</h1>
+                <MenuIcon className='navBarMenuIcon' onClick={showSidemenu} />
+                <h1 className='navBarTitle'>AI Financial</h1>
             </Navbar>
 
+
+
+
             <div className='sidemenu'>
+
+                <CloseIcon className='closeIcon' />
+
+
                 <ul className='sidemenuList'>
                     {SidemenuData.map((val, key) =>{
                         return (
