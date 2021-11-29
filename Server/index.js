d4253ca98aac1a0cb9de2710aa38a34c14bb1e14
NDC <andy_chen123@hotmail.com>
2021-11-29 17:29:02 -0500

    working on backend

--- Server/index.js
+++ Server/index.js
@@ -8,8 +8,6 @@ app.use(cors())
 app.use(express.json())
 
 
-
-
 // connect mySQL database
 const db = mysql.createConnection({
     host:'192.168.75.82',
@@ -34,19 +32,58 @@ db.connect(function(err){
 
 
 
-// insert directly from back end = use this for manual testing
-app.get('/insert',(req,res) => {
-    const sqlInsert = "INSERT INTO devnv (id, username, password) VALUES( '1', 'firstuser', 'password1');"
-    db.query(sqlInsert,(err, result) => {
-        if(err){
-            console.log(err)
-        }else{
-            res.send('working inserted')
-            console.log('console report working inserted')
-        }
+
+app.post('/insert', (req,res) =>  {
+    const firstname = req.body.firstName;
+    const lastname = req.body.lastName; 
+    const englishname = req.body.englishName;
+    const idarray = req.body.idArray;
+    const sqlInsertName = "INSERT INTO  person123 (firstname, lastname, englishname) VALUES (?,?,?)"
+    const sqlInsertIdArray = "INSERT INTO  idArray (idarray) VALUES (?)"
+
+    db.query(
+        sqlInsertName,
+        [firstname, lastname, englishname],
+        (err,result)=>{
+            if(err){
+                console.log(err)
+            } else{
+                res.send('value inserted')
+            }
+        })
+
+    db.query(
+        sqlInsertIdArray,
+        [idarray],
+        (err,result) => {
+            if(err){
+                console.log(err)
+            }else{
+                res.send('value inserted')
+            }
+        })  
     })
 
-})
+
+
+
+
+
+
+
+// // insert directly from back end = use this for manual testing
+// app.get('/insert',(req,res) => {
+//     const sqlInsert = "INSERT INTO devnv (id, username, password) VALUES( '1', 'firstuser', 'password1');"
+//     db.query(sqlInsert,(err, result) => {
+//         if(err){
+//             console.log(err)
+//         }else{
+//             res.send('working inserted')
+//             console.log('console report working inserted')
+//         }
+//     })
+
+// })
 
 
 
