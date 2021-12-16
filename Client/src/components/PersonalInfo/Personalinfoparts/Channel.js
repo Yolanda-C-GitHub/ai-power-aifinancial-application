7f51c8d99f21389fe149e334434cb9da193a46ea
NDC <andy_chen123@hotmail.com>
2021-12-16 15:08:47 -0500

    ss

--- Client/src/components/PersonalInfo/Personalinfoparts/Channel.js
+++ Client/src/components/PersonalInfo/Personalinfoparts/Channel.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import './channel.css'
+import {Card, Form, FloatingLabel} from 'react-bootstrap'
 
 export default function Channel() {
     return (
-        <div>
-            <h1>hi</h1>
-        </div>
+        <Card className='channelInfoCard'>
+            <Form.Group>
+                <FloatingLabel className='weChatID' label='WeChat ID'>
+                    <Form.Control placeholder='WeChat ID'
+                    />
+                </FloatingLabel>
+
+                <FloatingLabel className='telegramID' label='Telegram ID'>
+                    <Form.Control placeholder='Telegram ID'
+                    />
+                </FloatingLabel>
+            </Form.Group>
+        </Card>
     )
 }
