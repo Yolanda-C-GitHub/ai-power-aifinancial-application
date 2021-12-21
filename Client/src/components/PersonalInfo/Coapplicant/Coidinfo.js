ced1d0069df0d14ca187c4d5d0785d77994536ed
NDC <andy_chen123@hotmail.com>
2021-12-21 17:52:24 -0500

    use accordion for kyc;

--- Client/src/components/PersonalInfo/Coapplicant/Coidinfo.js
+++ Client/src/components/PersonalInfo/Coapplicant/Coidinfo.js
@@ -4,7 +4,10 @@ import './coidinfo.css'
 export default function Coidinfo() {
     return (
         <div>
+            
             <h1>working on issue with the validation where it wont validate hidden tabs in the form, possible solution need to be tested, call submit function everytime we change tabs, or add boolean as long as submit button is not clicked form should not validate (add addtional  boolean state default to false' when user clicks submit we will set this boolean to true.</h1>
+            <h1>use accordion for KYC</h1>
         </div>
+
     )
 }
