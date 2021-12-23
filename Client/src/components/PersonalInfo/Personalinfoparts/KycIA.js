58fe0092358fbae9464e3907caee6a3b44a17fb6
NDC <andy_chen123@hotmail.com>
2021-12-23 13:08:02 -0500

    KYC data complete

--- Client/src/components/PersonalInfo/Personalinfoparts/KycIA.js
+++ Client/src/components/PersonalInfo/Personalinfoparts/KycIA.js
@@ -186,9 +186,9 @@ export default function KycIA(params) {
                             <FloatingLabel className='kycquestions' label='2. What’s your goal for your investment portfolio?'>
                                 <Form.Control 
                                     placeholder='2. What’s your goal for your investment portfolio?'
-                                    value=''
+                                    value={params.personalInfoKYC.CL2}
                                     onChange={(e) => {
-                                        
+                                        params.personalInfoKYC.setCL2(e.target.value)
                                     }}
                                 />
                             </FloatingLabel>
@@ -196,9 +196,9 @@ export default function KycIA(params) {
                             <FloatingLabel className='kycquestions' label='3. How old are you?'>
                                 <Form.Control 
                                     placeholder='3. How old are you?'
-                                    value=''
+                                    value={params.personalInfoKYC.CL3}
                                     onChange={(e) => {
-                                        
+                                        params.personalInfoKYC.setCL3(e.target.value)
                                     }}
                                 />
                             </FloatingLabel>
@@ -206,9 +206,9 @@ export default function KycIA(params) {
                             <FloatingLabel className='kycquestions' label='4. What is your current annual family income (including pensions) before taxes?'>
                                 <Form.Control 
                                     placeholder='4. What is your current annual family income (including pensions) before taxes?'
-                                    value=''
+                                    value={params.personalInfoKYC.CL4}
                                     onChange={(e) => {
-                                        
+                                        params.personalInfoKYC.setCL4(e.target.value)
                                     }}
 
                                 />
@@ -217,9 +217,9 @@ export default function KycIA(params) {
                             <FloatingLabel className='kycquestions' label='5. After deducting any loan or mortgage balances, what is your immediate family’s overall net worth?'>
                                 <Form.Control 
                                     placeholder='5. After deducting any loan or mortgage balances, what is your immediate family’s overall net worth?'
-                                    value=''
+                                    value={params.personalInfoKYC.CL5}
                                     onChange={(e) => {
-                                        
+                                        params.personalInfoKYC.setCL5(e.target.value)
                                     }}
 
                                 />
@@ -228,9 +228,9 @@ export default function KycIA(params) {
                             <FloatingLabel className='kycquestions' label='6. When do you anticipate using this money?'>
                                 <Form.Control 
                                     placeholder='6. When do you anticipate using this money??'
-                                    value=''
-                                    onChange={(e) => {
-                                        
+                                    value={params.personalInfoKYC.CL6}
+                                    onChange={(e) => {  
+                                        params.personalInfoKYC.setCL6(e.target.value)
                                     }}
 
                                 />
@@ -239,9 +239,9 @@ export default function KycIA(params) {
                             <FloatingLabel className='kycquestions' label='7. When you need this money, how will you withdraw it?'>
                                 <Form.Control 
                                     placeholder='7. When you need this money, how will you withdraw it?'
-                                    value=''
+                                    value={params.personalInfoKYC.CL7}
                                     onChange={(e) => {
-                                        
+                                        params.personalInfoKYC.setCL7(e.target.value)
                                     }}
 
                                 />
@@ -250,9 +250,9 @@ export default function KycIA(params) {
                             <FloatingLabel className='kycquestions' label='8. What are your plans for withdrawals and/or additional contributions to your investments over the next five years?'>
                                 <Form.Control 
                                     placeholder='8. What are your plans for withdrawals and/or additional contributions to your investments over the next five years?'
-                                    value=''
+                                    value={params.personalInfoKYC.CL8}
                                     onChange={(e) => {
-                                        
+                                        params.personalInfoKYC.setCL8(e.target.value)
                                     }}
 
                                 />
@@ -261,9 +261,9 @@ export default function KycIA(params) {
                             <FloatingLabel className='kycquestions' label='9. Which best describes how much you know about investing?'>
                                 <Form.Control 
                                     placeholder='9. Which best describes how much you know about investing?'
-                                    value=''
+                                    value={params.personalInfoKYC.CL9}
                                     onChange={(e) => {
-                                        
+                                        params.personalInfoKYC.setCL9(e.target.value)
                                     }}
 
                                 />
@@ -272,20 +272,19 @@ export default function KycIA(params) {
                             <FloatingLabel className='kycquestions' label='10. If your investments lose significant value because of downturns in the market, how long are you prepared to hold your existing investments in anticipation of a recovery in value?'>
                                 <Form.Control 
                                     placeholder='10. If your investments lose significant value because of downturns in the market, how long are you prepared to hold your existing investments in anticipation of a recovery in value?'
-                                    value=''
+                                    value={params.personalInfoKYC.CL10}
                                     onChange={(e) => {
-                                        
+                                        params.personalInfoKYC.setCL10(e.target.value)
                                     }}
-
                                 />
                             </FloatingLabel>
 
                             <FloatingLabel className='kycquestions' label='11. Assuming that you are investing $100,000 for the long term, what is the maximum drop in your portfolio’s value that you could comfortably tolerate in any given year?'>
                                 <Form.Control 
                                     placeholder='11. Assuming that you are investing $100,000 for the long term, what is the maximum drop in your portfolio’s value that you could comfortably tolerate in any given year?'
-                                    value=''
+                                    value={params.personalInfoKYC.CL11}
                                     onChange={(e) => {
-                                        
+                                        params.personalInfoKYC.setCL11(e.target.value)                                        
                                     }}
 
                                 />
@@ -294,9 +293,9 @@ export default function KycIA(params) {
                             <FloatingLabel className='kycquestions' label='12. Which of the following statements would you feel most correctly describes your investment philosophy?'>
                                 <Form.Control 
                                     placeholder='12. Which of the following statements would you feel most correctly describes your investment philosophy?'
-                                    value=''
+                                    value={params.personalInfoKYC.CL12}
                                     onChange={(e) => {
-                                        
+                                        params.personalInfoKYC.setCL12(e.target.value)
                                     }}
 
                                 />
@@ -306,9 +305,9 @@ export default function KycIA(params) {
                                 <Form.Control 
                                     className='kycControlSize'
                                     placeholder='13. A portfolio is a basket of different investments. The returns earned by a portfolio depend on its mix of investments. The following graph shows the probable range of returns (from best to worst) of four hypothetical portfolios over a one-year period. Which of these portfolios wouldyou prefer to invest in?'
-                                    value=''
+                                    value={params.personalInfoKYC.CL13}
                                     onChange={(e) => {
-                                        
+                                        params.personalInfoKYC.setCL13(e.target.value)
                                     }}
 
                                 />
@@ -318,9 +317,9 @@ export default function KycIA(params) {
                                 <Form.Control 
                                     className='kycControlSize'
                                     placeholder='14. Some investors are more willing than others to accept periodic declines in the value of the portfolio as a trade-off for potentially higher long-term returns. Which response best represents your feelings toward the following statement?'
-                                    value=''
+                                    value={params.personalInfoKYC.CL14}
                                     onChange={(e) => {
-                                        
+                                        params.personalInfoKYC.setCL14(e.target.value)
                                     }}
 
                                 />
